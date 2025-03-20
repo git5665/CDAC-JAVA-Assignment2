281ed0c3a84c06d28c5deed16953e74ac9f56166
vipul kumar <122309708+git5665@users.noreply.github.com>
2025-03-20 18:23:45 +0530

    Add files via upload

---
+++ MainEmployees.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class MainEmployees {
+	public static void main(String arg[]) {
+		EmployeeMngt em = new EmployeeMngt();
+		
+		ArrayList<Employees> al1 = new ArrayList<>();
+		
+		
+	}
+
+}
